8876bd5dcfdd11234c0a6dfee341274e85de3aea
florence <qishioor@outlook.com>
2024-07-21 10:11:47 +0800

    update a little bug in program

---
+++ Upant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonM.logger;
+using log4net;
+using log4net.Config;
+using log4net.Core;
+
+
+namespace Upant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始 log");
+            string configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "conf", "log.xml");
+            FileInfo configFile = new FileInfo(configFilePath);
+            XmlConfigurator.Configure(configFile);
+            Console.WriteLine("log end");
+        }
+    }
+}
